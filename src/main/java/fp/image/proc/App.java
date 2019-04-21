e4c0bf584344f13829c1fad24c492b3478fe417b
LaOwlLol <something.less.than.nate@gmail.com>
2019-04-20 22:37:36 -0500

    add jar task to build. modified App.java inorder to correct path to resources at runtime when running from jar and command line during dev.

--- src/main/java/fp/image/proc/App.java
+++ src/main/java/fp/image/proc/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../../../views/App.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/App.fxml"));
         primaryStage.setTitle("Faux Pas Image Processor");
         primaryStage.setScene(new Scene(root, 1120, 860));
         primaryStage.show();
